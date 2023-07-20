b27287b07c14571fe670213bfcb79ec345f5373c
Mert Kureş <mertkures@hotmail.com>
2023-07-20 11:54:00 +0300

    Added UserRegisterViewModel

---
+++ Portfolyo/Areas/Writer/Models/UserRegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolyo.Areas.Writer.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage = "Lütfen kullanıcı adınızı yazın.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi yazın.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi tekrar yazın.")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Lütfen e-postanızı yazın.")]
+        public string Mail { get; set; }
+    }
+}
