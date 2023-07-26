3e0440779303479d0c56396023ba86b19d2db847
Mert Kureş <mertkures@hotmail.com>
2023-07-26 21:24:13 +0300

    Update Announcement.cs
    "Status" property replaced with "Type" property

--- EntityLayer/Concrete/Announcement.cs
+++ EntityLayer/Concrete/Announcement.cs
@@ -9,7 +9,7 @@ namespace EntityLayer.Concrete
         public int ID { get; set; }
         public string Title { get; set; }
         public DateTime Date { get; set; }
-        public bool Status { get; set; }
+        public string Type { get; set; }
         public string Content { get; set; }
     }
 }
