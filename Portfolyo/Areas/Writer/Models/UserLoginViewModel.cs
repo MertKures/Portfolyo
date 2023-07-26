d151fbbef3935b50fed9a12d2731e258cb57fa69
Mert Kureş <mertkures@hotmail.com>
2023-07-26 18:05:53 +0300

    Create UserLoginViewModel.cs

---
+++ Portfolyo/Areas/Writer/Models/UserLoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolyo.Areas.Writer.Models
+{
+    public class UserLoginViewModel
+    {
+        [Display(Name = "Kullanıcı adı")]
+        [Required(ErrorMessage = "Lütfen kullanıcı adınızı giriniz.")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz.")]
+        public string Password { get; set; }
+    }
+}
