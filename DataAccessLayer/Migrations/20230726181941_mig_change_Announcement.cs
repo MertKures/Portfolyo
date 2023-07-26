ad8b84fc454c5d32c1d9197f11b48da800ab4194
Mert Kureş <mertkures@hotmail.com>
2023-07-26 21:24:33 +0300

    Add migration

---
+++ DataAccessLayer/Migrations/20230726181941_mig_change_Announcement.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_change_Announcement : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Announcements");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Announcements",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Announcements");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "Announcements",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
