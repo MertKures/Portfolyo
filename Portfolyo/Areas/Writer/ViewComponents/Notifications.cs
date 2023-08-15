8dfe7029a4f6ed5561eb6e6a5cc6127e938a8c0e
Mert Kureş <mertkures@hotmail.com>
2023-08-15 15:07:39 +0300

    Created components for dashboard page
    * Notification and profile navigation bar code were moved from the dashboard page to their own components.
    * Profile picture is now being picked up from database.
    * Announcement titles are now listed in notifications area.

---
+++ Portfolyo/Areas/Writer/ViewComponents/Notifications.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Portfolyo.Areas.Writer.ViewComponents
+{
+    public class Notifications : ViewComponent
+    {
+        AnnouncementManager _announcementManager = new AnnouncementManager(new EfAnnouncementDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var result = _announcementManager.TGetList().Take(5).ToList();
+            return View(result);
+        }
+    }
+}
---
+++ Portfolyo/Areas/Writer/ViewComponents/ProfileNavbar.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Portfolyo.Areas.Writer.ViewComponents
+{
+    public class ProfileNavbar : ViewComponent
+    {
+        UserManager<WriterUser> _userManager;
+
+        public ProfileNavbar(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.ProfilePicture = user.ImageUrl;
+            return View();
+        }
+    }
+}
