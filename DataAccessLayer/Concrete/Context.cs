5e6dea29f0ee8bd7be365ff1df69d453738c646e
Mert Kureş <mertkures@hotmail.com>
2023-12-22 21:53:09 +0300

    added WriterMessage class

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -22,6 +22,7 @@ namespace DataAccessLayer.Concrete
         public DbSet<SocialMedia> SocialMedias { get; set; }
         public DbSet<Testimonial> Testimonials { get; set; }
         public DbSet<UserMessage> UserMessages { get; set; }
+        public DbSet<WriterMessage> WriterMessages { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<TodoList> TodoLists { get; set; }
         public DbSet<Announcement> Announcements { get; set; }
---
+++ EntityLayer/Concrete/WriterMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityLayer.Concrete
+{
+    public class WriterMessage
+    {
+        [Key]
+        public int WriterMessageID { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Subject { get; set; }
+        // "Content" ismi özel mesaj olarak algılandığından onu kullanmadık.
+        public string MessageContent { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
