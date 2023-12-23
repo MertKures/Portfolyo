7a9fd12e5f9a565741e2c2b793fc7a0f55e30175
Mert Kureş <mertkures@hotmail.com>
2023-12-23 14:48:44 +0300

    Updated MessageController to list messages from the database

--- Portfolyo/Areas/Writer/Controllers/MessageController.cs
+++ Portfolyo/Areas/Writer/Controllers/MessageController.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Portfolyo.Areas.Writer.Controllers
 {
     [Area("Writer")]
     public class MessageController : Controller
     {
-        public IActionResult Index()
+        WriterMessageManager _writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+
+        private readonly UserManager<WriterUser> _userManager;
+
+        public MessageController(UserManager<WriterUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index(string p)
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            p = values.Email;
+
+            var messageList = _writerMessageManager.TGetByFilter(p);
+
+            return View(messageList);
         }
     }
 }
