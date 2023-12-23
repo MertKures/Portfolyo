9a94e67f215ee954d51586182ae615bbe134ffbe
Mert Kureş <mertkures@hotmail.com>
2023-12-23 15:05:35 +0300

    Added filter functions to IWriterMessageService interface

--- BusinessLayer/Abstract/IWriterMessageService.cs
+++ BusinessLayer/Abstract/IWriterMessageService.cs
@@ -1,8 +1,11 @@
 ﻿using EntityLayer.Concrete;
+using System.Collections.Generic;
 
 namespace BusinessLayer.Abstract
 {
     public interface IWriterMessageService : IGenericService<WriterMessage>
     {
+        public List<WriterMessage> GetSendersMessages(string p);
+        public List<WriterMessage> GetReceiversMessages(string p);
     }
 }
